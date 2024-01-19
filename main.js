0790a1d372aef56aa7fd9a82251dd971a95888d7
amanDhiran <amandhiran201@gmail.com>
2024-01-19 21:06:41 +0530

    initial commit

---
+++ main.js
@@ -0,0 +1,30 @@
+const gallery = document.querySelector(".gallery");
+
+window.onmousemove = (e)=>{
+    const mouseX = e.clientX,
+        mouseY = e.clientY;
+
+    const xDecimal = mouseX/window.innerWidth;
+    const yDecimal = mouseY/window.innerHeight;
+
+    const maxX = gallery.offsetWidth - window.innerWidth,
+        maxY = gallery.offsetHeight - window.innerHeight;
+  
+  const panX = maxX * xDecimal * -1,
+        panY = maxY * yDecimal * -1;
+
+    
+    gallery.animate({
+        transform: `translate(${panX}px, ${panY}px`
+    }, {
+        duration: 4000,
+        fill: "forwards",
+        easing: "ease"
+    })
+}
+
+let intro = document.querySelector(".intro");
+
+setTimeout(() =>{
+    intro.style.top = "-100%"
+}, 2000)
